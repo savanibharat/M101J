ac0e8e10bc183bf7dcd97cd73f13cd2ee8709ca8
savanibharat <savanibharat@gmail.com>
2014-01-20 16:20:36 -0800

    FreeMarkerStyle Added

---
+++ src/main/java/com/tengen/HelloWorldFreeMarkerStyle.java
@@ -0,0 +1,39 @@
+package com.tengen;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+
+public class HelloWorldFreeMarkerStyle {
+
+	public static void main(String[] args) {
+
+		Configuration configuration = new Configuration();
+		configuration.setClassForTemplateLoading(HelloWorldSparkStyle.class,
+				"/");
+
+		try {
+			Template template = configuration
+					.getTemplate("/resources/hello.ftl");
+			StringWriter write = new StringWriter();
+
+			Map<String, Object> helloMap = new HashMap<String, Object>();
+			helloMap.put("name", "FreeMarker");/*
+												 * <html> <body> <h1> FreeMarker </h1>
+												 *  </body> </html>
+												 */
+
+			template.process(helloMap, write);
+
+			System.out.println(write);
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
