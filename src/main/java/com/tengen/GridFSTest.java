788f2f9e9454b9b46b5b55f84dbe9a3dff60a285
savanibharat <savanibharat@gmail.com>
2014-01-25 04:30:19 -0800

    GridFS output attached

--- src/main/java/com/tengen/GridFSTest.java
+++ src/main/java/com/tengen/GridFSTest.java
@@ -61,3 +61,63 @@ public class GridFSTest {
 		
 	}
 }
+/*
+ *> db.video.files.find().pretty()
+{
+        "_id" : ObjectId("52e390db5202b677a38b5cfe"),
+        "chunkSize" : NumberLong(262144),
+        "length" : NumberLong(31786618),
+        "md5" : "e2ea3e60af248679f62e291f148b9ae8",
+        "filename" : "BinarySearch.mp4",
+        "contentType" : null,
+        "uploadDate" : ISODate("2014-01-25T10:24:27.281Z"),
+        "aliases" : null,
+        "metadata" : {
+                "description" : "Binary Search",
+                "tags" : [
+                        "search",
+                        "data structures"
+                ]
+        }
+}
+--------------------------------------------------------------------
+of you run same program twice files will be like this.
+so be careful to use file names properly
+
+> db.video.files.find().pretty()
+{
+        "_id" : ObjectId("52e390db5202b677a38b5cfe"),
+        "chunkSize" : NumberLong(262144),
+        "length" : NumberLong(31786618),
+        "md5" : "e2ea3e60af248679f62e291f148b9ae8",
+        "filename" : "BinarySearch.mp4",
+        "contentType" : null,
+        "uploadDate" : ISODate("2014-01-25T10:24:27.281Z"),
+        "aliases" : null,
+        "metadata" : {
+                "description" : "Binary Search",
+                "tags" : [
+                        "search",
+                        "data structures"
+                ]
+        }
+}
+{
+        "_id" : ObjectId("52e391bc52023e879a4a105e"),
+        "chunkSize" : NumberLong(262144),
+        "length" : NumberLong(31786618),
+        "md5" : "e2ea3e60af248679f62e291f148b9ae8",
+        "filename" : "BinarySearch.mp4",
+        "contentType" : null,
+        "uploadDate" : ISODate("2014-01-25T10:28:12.406Z"),
+        "aliases" : null,
+        "metadata" : {
+                "description" : "Binary Search",
+                "tags" : [
+                        "search",
+                        "data structures"
+                ]
+        }
+}
+ * 
+ * */
