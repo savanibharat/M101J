d32822e51cf1895559ec2f04f3a6bd28c8cf5e47
savanibharat <savanibharat@gmail.com>
2014-01-25 04:45:34 -0800

    GridFs typo

--- src/main/java/com/tengen/GridFSTest.java
+++ src/main/java/com/tengen/GridFSTest.java
@@ -3,8 +3,6 @@ package com.tengen;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.IOException;
-import java.net.UnknownHostException;
 import java.util.ArrayList;
 
 import com.mongodb.BasicDBObject;
